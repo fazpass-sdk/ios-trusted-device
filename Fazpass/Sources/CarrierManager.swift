ec7d747e4e4fa9aee5e06fd593b5f30584b15477
Akbar PW <babaybabayan78@gmail.com>
2023-02-27 11:06:54 +0700

    add carrier manager

--- Fazpass/Sources/CarrierManager.swift
+++ Fazpass/Sources/CarrierManager.swift
@@ -9,5 +9,14 @@ import Foundation
 import CoreTelephony
 
 class CarrierManager {
-    
+    func getInfo(completion: @escaping (Dictionary<String?,Any?>?, CTCarrier?) -> Void) {
+        if #available(iOS 12.0, *) {
+            if let providers = CTTelephonyNetworkInfo().serviceSubscriberCellularProviders {
+                completion(providers, nil)
+            }
+        } else {
+            let provider = CTTelephonyNetworkInfo().subscriberCellularProvider
+            return completion(nil, provider)
+        }
+    }
 }
